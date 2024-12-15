8c10267f1340543cc8445b4ec7aceacd230be17c
winbo-yml-exe <winbo.yml@proton.me>
2024-12-15 09:22:22 +0100

    cdn: god im dumb

--- uploader.py
+++ uploader.py
@@ -29,7 +29,7 @@ def upload_file_using_git(repo_path, file_path, commit_message="cdn: Add file vi
 
     print(f"Your file ({file_name}) has been succesfully uploaded to the CDN!")
 
-    print("Your URL (may take some time to be deployed):", "https://cdn.winbo.is-a.dev/public/{file_name}")
+    print(f"Your URL (may take some time to be deployed): https://cdn.winbo.is-a.dev/public/{file_name}")
 
 file_path = input("Enter the path to the file to upload: ")
 
